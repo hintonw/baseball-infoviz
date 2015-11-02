167e3b054f061aad49584b55412c5492beafca59
ay0h <aoconnor7@gatech.edu>
2015-11-02 16:35:40 -0500

    HTML change
    Fixed up the field viz, added hover for each base, first base is click
    able displays text "YOU CLICKED ME"

--- util.js
+++ util.js
@@ -10,3 +10,17 @@ function pitcherType(d) {
 function teamType(d) {
   return d;
 }
+
+
+var tooltip = d3.select("#first-base")
+    .append("div")
+    .attr("class", "tooltip")
+    .text("a simple tooltip")
+    .on("mouseover", function(){return tooltip.style("visibility", "hidden");})
+	.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+
+
+function myFunction(){
+
+	document.getElementById("first-base").innerHTML = "YOU CLICKED ME!";
+}
