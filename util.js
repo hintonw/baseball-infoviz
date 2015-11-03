d020f91c0595b0263b4d51b147c61284aa0abd0c
ay0h <aoconnor7@gatech.edu>
2015-11-03 10:53:51 -0500

    More Html Fixes
    add SS circle, and Outfield circle, still need DH and figure out mouse
    over

--- util.js
+++ util.js
@@ -11,6 +11,16 @@ function teamType(d) {
   return d;
 }
 
+/*var tooltip = d3.select("#third-base")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden")
+    .text("a simple tooltip")
+    .on("mouseover", function(){return tooltip.style("visibility", "visible");})
+	.on("mousemove", function(){return tooltip.style("top",(d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+	.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+*/
 
 function myFunction(){
 
