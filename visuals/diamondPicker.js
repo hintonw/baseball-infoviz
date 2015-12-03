f8a55ce6b6b742c6e2a161419e5175cf9d6e8b77
Michael Maurer <mjmaurer7@gatech.edu>
2015-12-03 10:21:09 -0500

    Deleted unused files

--- visuals/diamondPicker.js
+++
@@ -1,3 +0,0 @@
-// NOTE: Do not use the same names for variables across files!!
-
-// Should update teamChart and playerChart and playerPicker when a team is selected using functions from those files
