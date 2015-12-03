63e5a571cf25fdc653493b8550a71c13ac07bd91
William Hinton <whinton3@gatech.edu>
2015-12-03 10:12:15 -0500

    Removed empty files

--- visuals/teamChart.js
+++
@@ -1,3 +0,0 @@
-// NOTE: Do not use the same names for variables across files!!
-
-// Team related bar chart that should have a listener for teamChanged or something like that
--- visuals/teamPicker.js
+++
@@ -1,3 +0,0 @@
-// NOTE: Do not use the same names for variables across files!!
-
-// Gets data from source/teams.js and draws team penants. Should update teamChart and playerChart and possibly playerPicker when a team is selected.
